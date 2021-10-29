19d55575e776accec74de39ed8204df04c319636
KacperMajkowski <kacper20015@o2.pl>
2021-10-29 11:48:25 +0200

    Commit1

--- src/main/java/com/majkowski/App.java
+++ src/main/java/com/majkowski/App.java
@@ -1,13 +1,7 @@
 package com.majkowski;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello Customer!");
     }
 }
