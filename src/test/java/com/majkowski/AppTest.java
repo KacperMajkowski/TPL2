0f8b7257c7665f5c82d4e32c2ac640d7d5ff2535
KacperMajkowski <kacper20015@o2.pl>
2021-11-05 08:53:52 +0100

    Test changes

--- src/test/java/com/majkowski/AppTest.java
+++ src/test/java/com/majkowski/AppTest.java
@@ -1,29 +1,38 @@
 package com.majkowski;
 
 import static org.junit.Assert.assertEquals;
+
+import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+    protected Client c;
+    
+    @Before
+    public void setUp() {
+        c = new Client("FirstNameTest", "LastNameTest");
+        c.inv.products.add(new Product("TestProduct1", 3, 4));
+        c.inv.products.add(new Product("TestProduct2", 5, 6));
+    }
+    
     @Test
     public void SumTest()
     {
-        Client c = new Client("FirstNameTest", "LastNameTest");
-        c.inv.products.add(new Product("TestProduct1", 3, 4));
-        c.inv.products.add(new Product("TestProduct2", 5, 6));
-    
         int total = 0;
         for (final Product p : c.inv.products) {
             total += p.price * p.count;
         }
-        assertEquals("Czy poprawnie suma rachunku",total, 3*4 + 5*6);
-        
+        Assert.assertEquals("Czy poprawnie suma rachunku", total, 3 * 4 + 5 * 6);
     }
-
+    
+    @After
+    public void tearDown() {
+        System.out.println("Tearing down");
+    }
 }
